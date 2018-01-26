071035216f09fbd02c13d08e9db4f5a8d1f44804
Guy Chappell <mettcat@gmail.com>
2018-01-26 00:12:36 -0800

    Mobile markup, styling and functionalitycomplete

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,27 @@ var uglifycss = require('gulp-uglifycss');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 var eslint = require('gulp-eslint');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var prettyError = require('gulp-prettyerror');
 
 
+//Gulp task for Sass
+gulp.task('sass', function() {
+  return gulp.src('./scss/style.scss')
+  .pipe(sass())
+  .pipe(prettyError())
+  .pipe(
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  )
+  .pipe(gulp.dest('./build/css'))
+  .pipe(cssnano())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('./build/css'));
+})
 
 //Gulp Lint
 gulp.task('lint', function() {
@@ -17,7 +36,7 @@ gulp.task('lint', function() {
 
 
 
-//Minify javascript files
+//Lint & Minify js files
 gulp.task('minify-js', gulp.series('lint', function(e) {  
   return gulp.src('./js/*.js')
     .pipe(uglify()) //call uglify function on files
@@ -25,20 +44,18 @@ gulp.task('minify-js', gulp.series('lint', function(e) {
     .pipe( gulp.dest('./build/js')); //move processed files to build folder
 }));
 
-//Minify css files
-gulp.task('minify-css', function(e) {  
-  return gulp.src('./css/*.css')
-    .pipe(uglifycss()) //call uglify function on files
-    .pipe(rename({extname: '.min.css'})) //rename the uglified files
-    .pipe( gulp.dest('./build/css')); //move processed files to build folder
-});
-
-
+// //Minify css files
+// gulp.task('minify-css', function(e) {  
+//   return gulp.src('./css/*.css')
+//     .pipe(uglifycss()) //call uglify function on files
+//     .pipe(rename({extname: '.min.css'})) //rename the uglified files
+//     .pipe( gulp.dest('./build/css')); //move processed files to build folder
+// });
 
 //Uglify CSS, JS files on change
 gulp.task('watch', function() {
   gulp.watch('js/*.js', gulp.series('minify-js'));
-  gulp.watch('css/*.css', gulp.series('minify-css'));
+  gulp.watch('scss/*.scss', gulp.series('sass'));
 });
 
 
@@ -53,7 +70,7 @@ gulp.task('browser-sync', function() {
     }
   });
 
-  gulp.watch(["*.html", "./build/css/*.css", "./build/js/*.js"]).on('change', browserSync.reload);
+  gulp.watch(["*.html", "./scss/*.scss", "./build/js/*.js"]).on('change', browserSync.reload);
 });
 
 
